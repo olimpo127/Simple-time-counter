f89d077e4e2dba261ccf7db146c5a6ec6629bffd
Felipe Ojeda <fojedai47@gmail.com>
2023-01-31 08:27:42 -0300

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let num1 = 0;
+  let num2 = 0;
+  let num3 = 0;
+  let num4 = 0;
+  let num5 = 0;
+  let num6 = 0;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='d-flex flex-row justify-content-center bg-black' id='timer'>
+      <i class="fa-solid fa-clock m-2 p-3 mb-2 bg-dark text-white"></i>
+      <p className='m-2 p-3 mb-2 bg-dark text-white'>{num1}</p>
+      <p className='m-2 p-3 mb-2 bg-dark text-white'>{num2}</p>
+      <p className='m-2 p-3 mb-2 bg-dark text-white'>{num3}</p>
+      <p className='m-2 p-3 mb-2 bg-dark text-white'>{num4}</p>
+      <p className='m-2 p-3 mb-2 bg-dark text-white'>{num5}</p>
+      <p className='m-2 p-3 mb-2 bg-dark text-white'>{num6}</p>
     </div>
   );
 }
