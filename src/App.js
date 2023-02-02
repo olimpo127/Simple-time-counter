b185f76f9ce7c2c7924d36d8e5c80c6f6aed6a0b
Felipe Ojeda <fojedai47@gmail.com>
2023-02-02 15:28:36 -0300

    simple counter working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import Timer from './components/Timer.js';
 
-function App() {
+function App(props) {
   return (
-    <div>
-      <Timer/>
-    </div>
+    <div className='d-flex flex-row justify-content-center bg-black' id='timer'>
+            <i class="fa-solid fa-clock m-2 p-3 mb-2 bg-dark text-white"></i>
+            <p className='m-2 p-3 mb-2 bg-dark text-white'>{props.num1 % 10}</p>
+            <p className='m-2 p-3 mb-2 bg-dark text-white'>{props.num2 % 10}</p>
+            <p className='m-2 p-3 mb-2 bg-dark text-white'>{props.num3 % 10}</p>
+            <p className='m-2 p-3 mb-2 bg-dark text-white'>{props.num4 % 10}</p>
+            <p className='m-2 p-3 mb-2 bg-dark text-white'>{props.num5 % 10}</p>
+            <p className='m-2 p-3 mb-2 bg-dark text-white'>{props.num6 % 10}</p>
+        </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let number = 0;
+setInterval(() => {
+  number++;
+  const one = Math.floor(number);
+  const ten = Math.floor(number/10);
+  const hundred = Math.floor(number/100);
+  const thousand = Math.floor(number/1000);
+  const tenThousand = Math.floor(number/10000);
+  const hundredThousand = Math.floor(number/100000);
+  root.render(
+    <React.StrictMode>
+      <App num1={hundredThousand} num2={tenThousand} num3={thousand} num4={hundred} num5={ten} num6={one}/>
+    </React.StrictMode>
+  );
+}, 1000);
+  
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
